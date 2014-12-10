b067712e051cebe7494bc2822e64247d302b6079
Stefan Heid <stefan-heid@web.de>
2014-12-10 19:14:48 +0100

    Now running without errors and result

--- src/controller/ImageToolbox.java
+++ src/controller/ImageToolbox.java
@@ -47,6 +47,6 @@ public class ImageToolbox {
 			}
 		}
 		
-		return null;
+		return pixelMatrix;
 	}
 }
--- src/model/functions/ImageDifference.java
+++ src/model/functions/ImageDifference.java
@@ -27,15 +27,32 @@ public class ImageDifference extends FitnessFunction {
 	@Override
 	public double getFittness(Individual i) {
 		int[][] source = ImageToolbox.pixelMatrix(MMAS.model.getInputImg());
-		int[][] actual = ImageToolbox.pixelMatrix(MMAS.model.getMutableImg()
-				.getImage());
+		int[][] actual = new int[source.length][source[0].length];
+
+		BitSet binCode = i.getBitSet();
+
+		// for all elements in bitcode, take 8 elements ant put it in a pixle
+		for (int x = 0; x < actual.length; x++) {
+			for (int y = 0; y < actual[0].length; y++) {
+				for (int bits = 0; bits < 8; bits++) {
+					int index = (x * actual.length + y) * actual[0].length
+							+ bits;
+					actual[x][y] = (int) (Math.pow(2, 8 - bits) * (binCode
+							.get(index) ? 1 : 0));
+
+				}
+			}
+
+		}
+		// TODO use individual i, otherwise it the result is proved to be wrong
 
 		double fitness = 0;
-		
+
 		for (int x = 0; x < source.length; x++) {
 			for (int y = 0; y < source[0].length; y++) {
 				// sum over all pixels 1-absolute error
-				fitness+=1.0-(Math.abs(source[x][y]-actual[x][y])/Math.pow(2, 8));
+				fitness += 1.0 - (Math.abs(source[x][y] - actual[x][y]) / Math
+						.pow(2, 8));
 
 			}
 		}
